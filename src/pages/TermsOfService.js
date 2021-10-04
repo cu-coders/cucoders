ba0f50e0e5ade2e8c50d5e440ae54b33c1593ad7
Ankit Raj <ankit.raj928@gmail.com>
2021-10-04 09:51:03 +0530

    Update TermsOfService.js

--- src/pages/TermsOfService.js
+++ src/pages/TermsOfService.js
@@ -7,7 +7,6 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import Header from "components/headers/light.js";
 import Footer from "components/footers/footers.js";
 import { SectionHeading } from "components/misc/Headings";
-const date = new Date();
 const HeadingRow = tw.div`flex`;
 const Heading = tw(SectionHeading)`text-gray-900 mb-10`;
 const Text = styled.div`
@@ -46,7 +45,7 @@ export default ({ headingText = "Terms And Condition" }) => {
           </HeadingRow>
           <Text>
 
-            <p>Last updated: {date} </p>
+            <p>Last updated: October 21, 2020 </p>
 
             <p>Please read these terms and conditions carefully before using Our Service.</p>
 
