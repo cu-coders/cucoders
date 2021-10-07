b1f3fb9861dd3dade9e231a80602a8d015e25445
RajeswarSharma <rjqwer47@gmail.com>
2021-10-07 10:55:28 +0530

    Cleaned temp files

--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -53,8 +53,15 @@ router.get("/user", (req, res) => {
     res.json({ username: null });
     console.log("Unknown User");
   } else {
-    res.json({ username: req.user.firstname });
-    console.log("User: " + req.user.firstname);
+    if(req.user.isactive){
+          res.json({ username: req.user.firstname });
+          console.log("User: " + req.user.firstname);
+    }
+    else{
+      console.log("Please confirm you mail first")
+      res.json({ mail_err: "Please confirm you mail first" });
+    }
+
   }
 });
 
