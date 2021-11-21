582dbd8640e09f4bf88e84d2e1177f774c621599
Ankit Raj <ankit.raj928@gmail.com>
2021-11-21 14:35:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Modal from "react-modal";
-import Honeybadger from '@honeybadger-io/js'
-import ErrorBoundary from '@honeybadger-io/react'
 
 Modal.setAppElement("#root");
-Honeybadger.configure({
-  apiKey: 'hbp_krEfC2Xc1ZovJBRIhHejq6WU3zFRNj2iu6yW',
-  environment: 'production'
-})
 
 ReactDOM.render(
-  <ErrorBoundary honeybadger={Honeybadger}>
-    <App />
-  </ErrorBoundary>,
-  document.getElementById('root')
-)
+  <App />,
+  document.getElementById("root")
+);
