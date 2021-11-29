a02a72a493a26684650081d3df6461978e047ee3
Ankit Raj <ankit.raj928@gmail.com>
2021-11-29 21:45:46 +0530

    Signup, login, forgot password
    Remove background and added nav bar

--- src/pages/lostPassword.js
+++ src/pages/lostPassword.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "components/headers/light.js";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import { Container as ContainerBase } from "components/misc/Layouts";
 import tw from "twin.macro";
@@ -8,7 +9,7 @@ import illustration from "images/login-illustration.svg";
 import logo from "images/logo.png";
 import { ReactComponent as LoginIcon } from "feather-icons/dist/icons/log-in.svg";
 
-const Container = tw(ContainerBase)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
+const Container = tw(ContainerBase)`min-h-screen text-white font-medium flex justify-center -m-8`;
 const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
 const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
 const LogoLink = tw.a``;
@@ -48,6 +49,8 @@ export default ({
 
 }) => (
   <AnimationRevealPage>
+  <Header />
+  <br />
     <Container>
       <Content>
         <MainContainer>
