5c20d02c2cc7f29534b3c09736d8b5296e14c95f
Restyled.io <commits@restyled.io>
2022-02-27 07:22:58 +0000

    Restyled by clang-format

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,30 +1,31 @@
-import React from "react";
-import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-import tw from "twin.macro";
-import { css } from "styled-components/macro"; //eslint-disable-line
-import Header from "components/headers/light.js";
-import Footer from "components/footers/footers.js";
-
-
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
 
-import { useEffect} from "react";
-import { useState } from "react";
-
-import styled from "styled-components";
-import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
-import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings";
+import Footer from "components/footers/footers.js";
+import Header from "components/headers/light.js";
+import {
+  SectionHeading,
+  Subheading as SubheadingBase
+} from "components/misc/Headings";
+import {Container, ContentWithPaddingXl} from "components/misc/Layouts.js";
 import {SectionDescription} from "components/misc/Typography";
-import { ReactComponent as InstagramIcon} from "images/instagram.svg";
-import { ReactComponent as LinkedinIcon} from "images/linkedin-icon.svg";
-import { ReactComponent as GithubIcon } from "images/github-icon.svg";
-const HeadingContainer = tw.div``
-const Heading = tw(SectionHeading)``
-const Subheading = tw(SubheadingBase)`text-center mb-3`
-const Description = tw(SectionDescription)`mx-auto text-center`
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import {ReactComponent as GithubIcon} from "images/github-icon.svg";
+import {ReactComponent as InstagramIcon} from "images/instagram.svg";
+import {ReactComponent as LinkedinIcon} from "images/linkedin-icon.svg";
+import React from "react";
+import {useEffect, useState} from "react";
+import Loader from "react-loader-spinner";
+import styled from "styled-components";
+import {css} from "styled-components/macro"; //eslint-disable-line
+import tw from "twin.macro";
 
-const Cards = tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`
+const HeadingContainer = tw.div``
+const Heading = tw(SectionHeading) ``
+const Subheading = tw(SubheadingBase) `text-center mb-3`
+const Description = tw(SectionDescription) `mx-auto text-center`
+
+const Cards =
+    tw.div`flex flex-wrap flex-row justify-center sm:max-w-2xl lg:max-w-5xl mx-auto`
 const Card = tw.div`mt-24 w-full sm:w-1/2 lg:w-1/3 flex flex-col items-center`
 const CardImage = styled.div`
   ${props => css`background-image: url("${props.imageSrc}");`}
@@ -43,7 +44,8 @@ const CardContent = styled.div`
 const CardLinks = styled.div`
   ${tw`mt-6 flex`}
   .link {
-    ${tw`mr-8 last:mr-0 text-gray-400 hocus:text-primary-500 transition duration-300`}
+    ${
+    tw`mr-8 last:mr-0 text-gray-400 hocus:text-primary-500 transition duration-300`}
     .icon {
       ${tw`fill-current w-6 h-6`}
     }
@@ -57,27 +59,27 @@ export default ({
   description = "It is impossible to do great things alone. For doing something big, one needs a team where each individual is doing what he's best at. Here's our strong team, working to get you all what we promise.",
 
 }) => {
-  const[t_cards,update_t_cards] = useState([])
+  const [t_cards, update_t_cards] = useState([])
   const [isLoading, setIsLoading] = useState(false);
-  
+
   useEffect(() => {
     setIsLoading(true);
     fetch("https://cucoders.herokuapp.com/api/team")
-      .then((res) => {
-        if (res.ok) {
-          console.log(res);
-          return res.json();
-        }
-      })
-      .then((result) => {
-        update_t_cards(result);
-        setIsLoading(false);
-      });
-
-  },[])
+        .then((res) => {
+          if (res.ok) {
+            console.log(res);
+            return res.json();
+          }
+        })
+        .then((result) => {
+          update_t_cards(result);
+          setIsLoading(false);
+        });
+  }, [])
   return (
   	<AnimationRevealPage>
-  	<Header isLoggedIn={isLoggedIn} />
+  	<Header isLoggedIn={
+    isLoggedIn} />
     <Container>
       <ContentWithPaddingXl>
         <HeadingContainer>
@@ -101,58 +103,45 @@ export default ({
                     marginTop: "50px",
                   }}
                 />
-              )}
-              { t_cards === undefined && (
+              )
+}
+{ t_cards === undefined && (
                 <div
-                  style={{
-                    fontSize: "18px",
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    color: "#6415ff",
-                    margin: "auto",
-                    marginBottom: "50px",
-                  }}
-                >
-                  Team Coming Soon!!
-                </div>
+  style =
+      {
+        {
+          fontSize: "18px", textAlign: "center", fontWeight: "bold",
+              color: "#6415ff", margin: "auto", marginBottom: "50px",
+        }
+      } > Team Coming Soon!
+      !</div>
               )}
               { t_cards !== undefined &&
                 t_cards.map((member, index) => (
                   <Card key={index}>
                     <CardImage imageSrc={member.profileImage} />
-                    <CardContent>
-                      <span className="fullname">{member.fullname}</span>
+      <CardContent><span className = "fullname">{member.fullname}<
+          /span>
                       <span className="role">{member.role}</span>
-                      <CardLinks>
-                      <a key={index} className="link" href={member.instagram}>
-                        <InstagramIcon style={{
-                          width: "20px",
-                          height: "20px",
-                        }} > 
-                        </InstagramIcon>
+      <CardLinks><a key = {index} className = "link" href = {member.instagram}>
+      <InstagramIcon style = {{ width: "20px", height: "20px", }}>
+      </InstagramIcon>
                     </a>
-                    <a key={index} className="link" href={member.linkedin}> 
-                        <LinkedinIcon style={{
-                          width: "20px",
-                          height: "20px",
-                        }} >
-                        </LinkedinIcon>
+      <a key = {index} className = "link" href = {member.linkedin}>
+      <LinkedinIcon style = {{ width: "20px", height: "20px", }}>
+      </LinkedinIcon>
                     </a>
-                    <a key={index} className="link" href={member.github}>
-                        <GithubIcon style={{
-                          width: "20px",
-                          height: "20px",
-                        }} >
-                        </GithubIcon>
+      <a key = {index} className = "link" href = {member.github}>
+      <GithubIcon style = {{ width: "20px", height: "20px", }}>
+      </GithubIcon>
                     </a>
-                    </CardLinks>
+      </CardLinks>
                     </CardContent>
-                  </Card>
+      </Card>
                 ))}
             </Cards>
       </ContentWithPaddingXl>
-    </Container>
-    <Footer />
-    </AnimationRevealPage>
+    </Container><Footer /><
+      /AnimationRevealPage>
   );
 }
